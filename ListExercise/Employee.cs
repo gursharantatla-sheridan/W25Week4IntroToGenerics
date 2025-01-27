159b11cae3bfd88efe47057226c398cd6eef1635
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-27 13:52:31 -0500

    list ex

---
+++ ListExercise/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercise
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}\nName = {Name}\nSalary = {Salary:C}\n\n";
+        }
+    }
+}
---
+++ ListExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ListExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(101, "John", 5000);
+            Employee emp2 = new Employee(102, "Anne", 7000);
+            Employee emp3 = new Employee(103, "Mark", 3000);
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(emp1);
+            employees.Add(emp2);
+            employees.Add(emp3);
+
+            employees.Insert(1, emp3);
+            employees.Remove(emp3);
+            //employees.Clear();
+
+            foreach (var emp in employees)
+                Console.WriteLine(emp);
+
+
+            // searching 
+
+
+        }
+    }
+}
